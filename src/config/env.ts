153b5bf97bee599f97334f349c2039e697be47b9
S00251709 <S00251709@atu.ie>
2026-09-23 10:50:54 +0100

    Worksheet3Task2&3

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,5 +4,7 @@ dotenv.config();
 
 export const env = {
     port: Number(process.env.PORT ?? 3001),
-    nodeEnv: process.env.NODE_ENV ?? 'development'
+    nodeEnv: process.env.NODE_ENV ?? 'development',
+    mongoURI: process.env.MONGODB_URI ?? ""
 }
+
